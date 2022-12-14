5a2c5c3ac86fda11a649f32c198cfea292f480db
kdevine <kdevine@yahoo.com>
2022-12-14 17:55:45 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nativescript-stackblitz-templates-8qw4by
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nativescript-stackblitz-templates-8qw4by)
---
+++ app/App.svelte
@@ -0,0 +1,7 @@
+<frame id="rootFrame" defaultPage="{Home}">
+    <Home />
+</frame>
+
+<script lang="ts">
+    import Home from './components/Home.svelte'
+</script>
